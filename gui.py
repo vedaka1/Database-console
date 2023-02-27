e4a1afecfd36cb88a36a5dae36ed0abf886c2f63
vedaka1 <vladik.kuleshov.2003@mail.ru>
2023-02-27 22:27:47 +0300

    collumn data view

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys, traceback
 from PyQt6 import uic, QtWidgets
-from main_console import print_register, add_register, delete_register, update_register
+from main_console import print_register, add_register, delete_register, update_register, connection
 
 
 class Update_dialog(QtWidgets.QDialog):
@@ -34,18 +34,29 @@ class Main(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Main, self).__init__()
-        uic.loadUi("main.ui", self)
+        uic.loadUi("main_test.ui", self)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode.Stretch)
         self.delete_register_btn.clicked.connect(self.delete_reg)
         self.update_register_btn.clicked.connect(self.update_reg)
         self.add_register_btn.clicked.connect(self.add_reg)
-        self.print_reg()
+        self.load_data()
 
-    def print_reg(self):
+    def load_data(self):
         try:
-            text = ''
-            self.info_field.setText(print_register(text))
+            x = 0
+            items = print_register(x)
+            tablerow = 0
+            self.tableWidget.setRowCount(len(items))
+            for row in items:
+                x = [str(i) for i in list(row)]
+                self.tableWidget.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(x[0]))
+                self.tableWidget.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(x[1]))
+                self.tableWidget.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(x[2]))
+                self.tableWidget.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(x[3]))
+                self.tableWidget.setItem(tablerow, 4, QtWidgets.QTableWidgetItem(x[4]))
+                tablerow += 1
         except:
-            print('\033[31m -- print_reg Error -- \033[0m')
+            print('\033[31m -- load_data Error -- \033[0m', traceback.format_exc())
 
     def add_reg(self):
         dlg = Add_dialog()
@@ -55,7 +66,7 @@ class Main(QtWidgets.QMainWindow):
                              str(dlg.discipline_name_field.text()),
                              int(dlg.teacher_id_field.text()),
                              int(dlg.mark_field.text()))
-                self.print_reg()
+                self.load_data()
                 print('Success add_reg, id')
             else:
                 print('Canceled add_reg')
@@ -69,7 +80,7 @@ class Main(QtWidgets.QMainWindow):
             if dlg.exec():
                 delete_register(int(dlg.id_field.text()))
                 print("Success delete_reg, id", int(dlg.id_field.text()))
-                self.print_reg()
+                self.load_data()
             else:
                 print("Canceled delete_reg")
         except:
@@ -82,8 +93,7 @@ class Main(QtWidgets.QMainWindow):
             if dlg.exec():
                 update_register(int(dlg.id_field.text()),
                                 int(dlg.mark_field.text()))
-                self.print_reg()
-                print('Success update_reg, id: ', int(dlg.id_field.text()))
+                self.load_data()
                 print('Success update_reg, id: ', int(dlg.id_field.text()))
             else:
                 print('Canceled update_dialog')
